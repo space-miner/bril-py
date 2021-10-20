16e1e6a5d8e0c0da95c9b86a26e4d683b2ec9830
tchow <tchow@tutamail.com>
2021-10-20 01:40:24 -0400

    add labels to the output bril program

--- dce.py
+++ dce.py
@@ -1,12 +1,12 @@
 import json
 import sys
+import cfg
 
-
-def remove_reassigned_variables(instructions):
+def remove_reassigned_variables(basic_block):
     var_map = {}
     unused = set()
     remove = set()
-    for i, instruction in enumerate(instructions):
+    for i, instruction in enumerate(basic_block):
         if "args" in instruction:
             for var in instruction["args"]:
                 if var in var_map:
@@ -20,11 +20,11 @@ def remove_reassigned_variables(instructions):
                     remove.add(i)
             var_map[var] = i
             unused.add(i)
-    modified_instructions = []
-    for i, instruction in enumerate(instructions):
+    modified_basic_block = []
+    for i, instruction in enumerate(basic_block):
         if i not in remove:
-            modified_instructions.append(instruction)
-    return modified_instructions
+            modified_basic_block.append(instruction)
+    return modified_basic_block
 
                  
 def remove_dead_code(instructions):
@@ -45,14 +45,22 @@ def remove_dead_code(instructions):
             for i, instruction in enumerate(instructions):
                 if i not in remove:
                     modified_instructions.append(instruction)
-        instructions = modified_instructions
+            instructions = modified_instructions
     return instructions
 
 
 def simple_dead_code_elimination(instructions):
     instructions = remove_dead_code(instructions)
-    instructions = remove_reassigned_variables(instructions)
-    return instructions
+    labeled_blocks = cfg.make_labeled_blocks(instructions)
+    basic_blocks = []
+    for (label, block) in labeled_blocks:
+        basic_blocks.append([{"label": label}])
+        basic_blocks.append(block)
+    modified_instructions = []
+    for block in basic_blocks:
+        modified_block = remove_reassigned_variables(block)
+        modified_instructions.extend(modified_block)
+    return modified_instructions
 
 
 if __name__ == "__main__":
@@ -61,4 +69,4 @@ if __name__ == "__main__":
     for function in functions:
         instructions = function["instrs"]
         function["instrs"] = simple_dead_code_elimination(instructions)
-    print(repr(prog).replace('\'', '"'))
+    print(json.dumps(prog, indent=4, sort_keys=True))
